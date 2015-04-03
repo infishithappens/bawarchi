43f7c2620d0ccfdf407f56cf8427ab1e61745c2c
user.name <infishithappens@gmail.com>
2015-04-03 23:43:24 +0800

    First Commit

---
+++ _templates/includes/footer.php
@@ -0,0 +1,18 @@
+        <!-- build:js //ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js -->
+        <script src="/assets/bawarchi/js/vendor/jquery-1.11.1.js"></script>
+        <!-- /build -->
+
+        <!-- build:js /assets/bawarchi/js/plugins.min.js -->
+        <script src="/assets/bawarchi/js/plugins.js"></script>
+        <!-- /build -->
+
+        <!-- build:js /assets/bawarchi/js/modules.min.js -->
+        <script src="/assets/bawarchi/js/modules.js"></script>
+        <!-- /build -->
+
+        <!-- build:js /assets/bawarchi/js/main.min.js -->
+        <script src="/assets/bawarchi/js/main.js"></script>
+        <!-- /build -->
+
+    </body>
+</html>
